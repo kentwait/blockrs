6a5f7ec7cf146f8468e45ffca33b23fb0431ed1c
kent <kentkawashima@gmail.com>
2018-12-13 16:15:15 +0900

    Adds makeblastdb and matching functions

--- evogen/blast.py
+++ evogen/blast.py
@@ -3,6 +3,96 @@
 """
 import pandas as pd
 import numpy as np
+import subprocess as proc
+from evogen.reader import read_blast_table
+
+
+MAKEBLASTDB_CMD_TEMPLATE = 'makeblastdb -in {ffn} -input_type fasta -dbtype ' \
+                           'nucl -out {out}'
+BLASTN_CMD_TEMPLATE = 'blastn -task {task} -query {query} -db {db} ' \
+                      '-out {report} -outfmt "{outfmt}" ' \
+                      '-max_target_seqs 1 -num_threads {threads}'
+BLASTN_TBL_TEMPLATE = '6 std qlen slen qcovs sstrand'
+
+
+def make_blast_db(path, output_path):
+    """Runs makeblastdb command within Python.
+    Assumes that makeblastdb is in $PATH and can be called anywhere.
+
+    Parameters
+    ----------
+    path : str
+    output_path : str
+
+    Returns
+    -------
+    int
+        Return code. Returns 0 if makeblastdb successfully finished.
+
+    Raises
+    ------
+    proc.CalledProcessError
+        If the return code from running makeblastdb is non-zero, raises a
+        CalledProcessError.
+
+    """
+    cmd = MAKEBLASTDB_CMD_TEMPLATE.format(ffn=path, out=output_path)
+    try:
+        p = proc.run(cmd.split(), check=True)
+    except proc.CalledProcessError as e:
+        raise e
+    return p.returncode
+
+
+def blastn_match(fasta_path, db_path, report_path,
+                 task='blastn', outfmt='6 std qlen slen qcovs sstrand',
+                 threads=1):
+    """Runs BLAST search within Python using the blastn command.
+    Assumes that blastn is in $PATH and can be called anywhere.
+
+    Parameters
+    ----------
+    fasta_path : str
+        Path to the fasta file used as query.
+    db_path : str
+        Path to the database files created by makeblastdb.
+    report_path : str
+        Path to where to store blast results.
+    task : str, optional
+        blastn task or mode to sue. By default, uses "blastn".
+        Other tasks include "blastn-short", "megablast", and "dc-megablast".
+    outfmt : str, optional
+        Output format string. By default, uses the table format (6) with
+        additional columns "qlen", "slen", "qcovs", "sstrand".
+    threads : int, optional
+        Number of threads blastn will use. It is recommended to change this
+        to the number of cores in the computer.
+
+    Returns
+    -------
+    pandas.DataFrame
+        Blast
+
+    Raises
+    ------
+    proc.CalledProcessError
+        If the return code from running blastn is non-zero, raises a
+        CalledProcessError.
+
+    """
+    cmd = BLASTN_CMD_TEMPLATE.format(
+        task=task,
+        query=fasta_path,
+        db=db_path,
+        report=report_path,
+        outfmt=outfmt,
+        threads=threads
+    )
+    try:
+        proc.run(cmd.split(), check=True)
+    except proc.CalledProcessError as e:
+        raise e
+    return read_blast_table(report_path)
 
 
 def summarize_group(group):
@@ -115,7 +205,10 @@ def summarize_blast_df(df, summarize_func=summarize_group,
     return filtered_summ_df
 
 
-def reciprocal_blast_match_df(forward_blast_df, reverse_blast_df, join='inner'):
+def reciprocal_blast_match_df(forward_blast_df, reverse_blast_df, join='inner',
+                              col_labels=['pident', 'length', 'mismatch',
+                                          'gapopen', 'bitscore', 'qlen',
+                                          'slen']):
     """Returns a concatenated DataFrame containg the forward and reverse
     blast matches joined by an inner join.
 
@@ -136,14 +229,13 @@ def reciprocal_blast_match_df(forward_blast_df, reverse_blast_df, join='inner'):
                                 for label in forward_blast_df.columns]
     reverse_blast_df.columns = ['r_' + label
                                 for label in reverse_blast_df.columns]
+    reverse_blast_df = reverse_blast_df.reorder_levels(['saccver', 'qaccver'])
 
     # Concat df
     concat_df = pd.concat([forward_blast_df, reverse_blast_df],
                           axis=1, join=join)
     concat_df.columns = pd.MultiIndex.from_product(
-        [['forward', 'reverse'],
-         ['pident', 'length', 'mismatch', 'gapopen', 'bitscore', 'qlen', 'slen']
-        ],
+        [['forward', 'reverse'], col_labels],
         names=['match', 'property']
     )
     return concat_df
