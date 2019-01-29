1d8c91add1815e9f3cd9c19eee4db6b6d268d154
kent <kentkawashima@gmail.com>
2019-01-29 12:34:54 +0900

    Edits setup.py

--- setup.py
+++ setup.py
@@ -1,92 +1,28 @@
-# -*- coding: utf-8 -*-
-import distutils.util
-import os
-import re
-import subprocess
-from ast import literal_eval
-from setuptools import setup
+import setuptools
 from setuptools_rust import Binding, RustExtension
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-def module_attr_re(attr):
-    return re.compile(r'__{0}__\s*=\s*(.*)'.format(attr))
-
-
-def grep_attr(body, attr):
-    return literal_eval(module_attr_re(attr).search(body).group(1))
-
-
-def parse_module_metadata():
-    with open("blockrs/__init__.py", "r") as f:
-        body = f.read()
-        return [grep_attr(body, attr) for attr in ("version", "author")]
-
-
-def strtobool(string):
-    return bool(distutils.util.strtobool(string))
-
-
-def git_rev_count(revision):
-    return subprocess.check_output(["git",
-                                    "rev-list",
-                                    "--count",
-                                    revision]).decode("utf-8").strip()
-
-
-# def expand_version(version):
-#     env = os.environ
-#     is_ci = strtobool(env.get("CI", "f"))
-#     pr_sha = env.get("TRAVIS_PULL_REQUEST_SHA") or \
-#              env.get("APPVEYOR_PULL_REQUEST_HEAD_COMMIT")
-#     branch = env.get("APPVEYOR_REPO_BRANCH") or env.get("TRAVIS_BRANCH")
-#     if is_ci and not pr_sha and branch == "master":
-#         commit = env.get("APPVEYOR_REPO_COMMIT") or env.get("TRAVIS_COMMIT")
-#         rev_count = git_rev_count(commit)
-#         return "{}.dev{}".format(version, rev_count)
-#     return version
-
-
-def main():
-    version, author = parse_module_metadata()
-    description = "A simple, cross-platform GUI automation library for Python."
-    setup(
-        name='blockrs',
-        # version=expand_version(version),
-        version='0.3.0',
-        # author=author,
-        # author_email='kentkawashima@gmail.com',
-        # description=description,
-        # license='Apache-2.0',
-        # classifiers=[
-        #     'Development Status :: 5 - Production/Stable',
-        #     'Operating System :: MacOS',
-        #     'Operating System :: MacOS :: MacOS X',
-        #     'Environment :: Win32 (MS Windows)',
-        #     'Environment :: X11 Applications',
-        #     'Intended Audience :: Developers',
-        #     'Natural Language :: English',
-        #     'Operating System :: MacOS :: MacOS X',
-        #     'Operating System :: Microsoft :: Windows',
-        #     'Operating System :: POSIX :: Linux',
-        #     'Programming Language :: Rust',
-        #     'Programming Language :: Python :: 2.7',
-        #     'Programming Language :: Python :: 3',
-        # ],
-        # keywords=[
-        #     "autopy",
-        #     "autopilot",
-        #     "GUI",
-        #     "automation",
-        #     "cross-platform",
-        # ],
-        # platforms=["macOS", "Windows", "X11"],
-        rust_extensions=[
-            RustExtension('libblockrs.block', 'Cargo.toml', binding=Binding.PyO3),
-        ],
-        packages=['blockrs'],
-        zip_safe=False,  # Rust extensions are not zip safe, like C-extensions.
-    )
-
-
-if __name__ == '__main__':
-    main()
+setuptools.setup(
+    name='blockrs',
+    version='0.3.0.post3',
+    author='Kent Kawashima',
+    author_email='kentkawashima@gmail.com',
+    description='Encode and decode alignment blocks',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/kentwait/blockrs',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    keywords=['block', 'alignment', 'bioinformatics'],
+    rust_extensions=[
+        RustExtension('libblockrs.block', 'Cargo.toml', binding=Binding.PyO3),
+    ],
+    packages=['blockrs'],
+    requires=[],
+    zip_safe=False,  # Rust extensions are not zip safe, like C-extensions.
+)
